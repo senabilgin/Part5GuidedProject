768caec5648a537959b196dc2d988e09ecc74b33
senabilgin <49808970+senabilgin@users.noreply.github.com>
2025-02-20 12:12:36 -0500

    unit 4 /8

--- Program.cs
+++ Program.cs
@@ -1,8 +1,37 @@
 using System;
 
-string[] pettingZoo = 
+string[] pettingZoo =
 {
-    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese", 
-    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese",
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
     "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
 };
+
+// RandomizeAnimals();
+
+
+// string[,] group = AssingGroup();
+
+Console.WriteLine("School A");
+
+// PrintGroup(group);
+
+void RandomizeAnimals() 
+{
+    Random random = new Random();
+
+    for (int i = 0; i < pettingZoo.Length; i++) 
+    {
+        int r = random.Next(i, pettingZoo.Length);
+
+        string temp = pettingZoo[r];
+        pettingZoo[r] = pettingZoo[i];
+        pettingZoo[i] = temp;
+    }
+}
+
+foreach(string animal in pettingZoo) 
+{
+    Console.WriteLine(animal);
+}
+
