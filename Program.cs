45f859e9e3e6a84eb6854c1016b6aa03da58ce41
senabilgin <49808970+senabilgin@users.noreply.github.com>
2025-02-20 12:15:26 -0500

    unit 5 of 8

--- Program.cs
+++ Program.cs
@@ -30,8 +30,18 @@ void RandomizeAnimals()
     }
 }
 
-foreach(string animal in pettingZoo) 
+string[,] AssignGroup(int groups = 6) 
 {
-    Console.WriteLine(animal);
-}
+    string[,] result = new string[groups, pettingZoo.Length/groups];
+    int start = 0;
 
+    for (int i = 0; i < groups; i++) 
+    {
+        for (int j = 0; j < result.GetLength(1); j++) 
+        {
+            result[i,j] = pettingZoo[start++];
+        }
+    }
+
+    return result;
+}
