84dd90e7c9309fb6dbd0a41a060a3bf34f533c63
senabilgin <49808970+senabilgin@users.noreply.github.com>
2025-02-20 12:02:31 -0500

    unit3/8

---
+++ Program.cs
@@ -0,0 +1,8 @@
+using System;
+
+string[] pettingZoo = 
+{
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese", 
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+};
